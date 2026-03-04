9a69f59e89cffa773121d925456ff445cb9734d6
menju2ikg8uti <menju2ikg8uti@kolabnow.com>
2026-03-04 18:47:06 +0700

    Update and rename boolean.py to 3.py

--- python/3.py
+++ python/3.py
@@ -2,22 +2,7 @@ from visma.simplify.simplify import simplify
 from visma.functions.constant import Constant
 from visma.io.tokenize import tokenizer
 
-# TODO: Test cases.
-# TODO: Implement GUI/CLI.
-
-
 def logicalAND(token1, token2):
-    """Implements Bitwise AND
-    Arguments:
-        token1 -- {list} -- List of tokens of a constant number
-        token2 -- {list} -- List of tokens of a constant number
-
-    Returns:
-        token_string {string} -- final result stored in a string
-        animation {list} -- list of equation solving process
-        comments {list} -- list of comments in equation solving process
-    """
-
     comments = []
     animations = []
     token1, _, _, _, _ = simplify(token1)
@@ -43,17 +28,6 @@ def logicalAND(token1, token2):
 
 
 def logicalOR(token1, token2):
-    """Implements Bitwise OR
-    Arguments:
-        token1 -- {list} -- List of tokens of a constant number
-        token2 -- {list} -- List of tokens of a constant number
-
-    Returns:
-        token_string {string} -- final result stored in a string
-        animation {list} -- list of equation solving process
-        comments {list} -- list of comments in equation solving process
-    """
-
     comments = []
     animations = []
     token1, _, _, _, _ = simplify(token1)
@@ -79,16 +53,6 @@ def logicalOR(token1, token2):
 
 
 def logicalNOT(token1):
-    """Implements Bitwise NOT
-    Arguments:
-        token1 -- {list} -- List of tokens of a constant number
-
-    Returns:
-        token_string {string} -- final result stored in a string
-        animation {list} -- list of equation solving process
-        comments {list} -- list of comments in equation solving process
-    """
-
     comments = []
     animations = []
     token1, _, _, _, _ = simplify(token1)
